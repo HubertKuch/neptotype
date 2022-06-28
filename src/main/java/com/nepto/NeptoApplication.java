6b76b6ba4090db7c9eef0feeec1f98840a728ea8
hubert <kuchhubert@gmail.com>
2022-06-28 21:19:52 +0200

    init

---
+++ src/main/java/com/nepto/NeptoApplication.java
@@ -0,0 +1,13 @@
+package com.nepto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeptoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nepto/NeptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nepto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
